45717daa7a389f64a3816a5a2549f349015c6e8b
AbhayThapZ <abhaythapliyal415@gmail.com>
2026-08-11 07:04:09 +0530

    Lecture 105

---
+++ Lecture 105/01_Merge_two_BSTs.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+//Insert a node in BST
+Node* insert(Node* root,int val){
+    if(root==NULL){
+        return new Node(val);
+    }
+    if(val<root->data){
+        root->left = insert(root->left,val);
+    }
+    else{
+        root->right = insert(root->right,val);
+    }
+    return root;
+}
+
+Node* buildBST(vector<int> &arr){
+    Node* root = NULL;
+    for(int val:arr){
+        root=insert(root,val);
+    }
+    return root;
+}
+
+void inorder(Node* root, vector<int> &seq){
+    if(root==NULL){
+        return ;
+    }
+    inorder(root->left, seq);
+    seq.push_back(root->data);
+    inorder(root->right, seq);
+}
+
+Node* buildBSTFromSorted(vector<int> &arr,int start,int end){
+    if(start>end){
+        return NULL;
+    }
+    int mid=start+(end-start)/2;
+    Node* root = new Node(arr[mid]);
+    root->left = buildBSTFromSorted(arr,start,mid-1);
+    root->right = buildBSTFromSorted(arr,mid+1,end);
+    return root;
+}
+
+Node* merge2BSTs(Node* root1,Node* root2){
+    vector<int> arr1,arr2;
+    inorder(root1, arr1);
+    inorder(root2, arr2);
+    vector<int> temp;//final bst inorder sequence
+    int i=0,j=0;
+    while(i<arr1.size() && j<arr2.size()){
+        if(arr1[i]<arr2[j]){
+            temp.push_back(arr1[i++]);
+        }else{
+            temp.push_back(arr2[j++]);
+        }
+    }
+    while(i<arr1.size()){
+        temp.push_back(arr1[i++]);
+    }
+    while(j<arr2.size()){
+        temp.push_back(arr2[j++]);
+    }
+    return buildBSTFromSorted(temp,0,temp.size()-1);
+}
+
+int main(){
+    vector<int> arr1 = {8,2,1,10};
+    vector<int> arr2 = {5,3,0};
+    Node* root1 = buildBST(arr1);
+    Node* root2 = buildBST(arr2);
+    Node* root = merge2BSTs(root1, root2);
+    vector<int> seq;
+    inorder(root, seq);
+    for(int val:seq){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
