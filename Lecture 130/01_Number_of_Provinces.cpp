71c5cd4c1cf97033dcae125378d71f3101d9a389
AbhayThapZ <abhaythapliyal415@gmail.com>
2026-08-27 21:12:58 +0530

    Lecture 130

---
+++ Lecture 130/01_Number_of_Provinces.cpp
@@ -0,0 +1,51 @@
+//Leetcode 547
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(int i, vector<vector<int>>& adj, vector<bool>& vis) {
+        vis[i] = true;
+
+        for (int j = 0; j < adj[i].size(); j++) {
+            if (adj[i][j] == 1 && !vis[j]) {
+                dfs(j, adj, vis);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int numOfProvinces = 0;
+        int n = isConnected.size();
+
+        vector<bool> vis(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(i, isConnected, vis);
+                numOfProvinces++;
+            }
+        }
+
+        return numOfProvinces;
+    }
+};
+
+int main() {
+
+    vector<vector<int>> isConnected = {
+        {1, 1, 0,0},
+        {1, 1, 0,0},
+        {0, 0, 1,1},
+        {0,0,1,1}
+    };
+
+    Solution obj;
+
+    int answer = obj.findCircleNum(isConnected);
+
+    cout << "Number of Provinces: " << answer << endl;
+
+    return 0;
+}
