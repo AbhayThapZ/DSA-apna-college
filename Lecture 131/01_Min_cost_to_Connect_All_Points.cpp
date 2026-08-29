f4a35b2577d95cb76a0172263c23bdd7f42afcf8
AbhayThapZ <abhaythapliyal415@gmail.com>
2026-08-29 18:36:33 +0530

    Lecture 131 132

---
+++ Lecture 131/01_Min_cost_to_Connect_All_Points.cpp
@@ -0,0 +1,76 @@
+//Leetcode 1584
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int manDist(vector<vector<int>>& points, int p1, int p2) {
+        return abs(points[p1][0] - points[p2][0]) +
+               abs(points[p1][1] - points[p2][1]);
+    }
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+
+        // Priority Queue -> Min Heap
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > pq;
+
+        vector<bool> mstSet(n, false);
+        int mstCost = 0;
+
+        // {weight, node}
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+            auto p = pq.top();
+
+            int wt = p.first;
+            int node = p.second;
+
+            pq.pop();
+
+            if (mstSet[node])
+                continue;
+
+            mstSet[node] = true;
+            mstCost += wt;
+
+            for (int i = 0; i < n; i++) {
+                if (!mstSet[i]) {
+                    int edgeWt = manDist(points, node, i);
+                    pq.push({edgeWt, i});
+                }
+            }
+        }
+
+        return mstCost;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    vector<vector<int>> points = {
+        {0, 0},
+        {2, 2},
+        {3, 10},
+        {5, 2},
+        {7, 0}
+    };
+
+    int result = obj.minCostConnectPoints(points);
+
+    cout << "Minimum Cost = " << result << endl;
+
+    return 0;
+}
